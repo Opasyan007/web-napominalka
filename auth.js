fddee9a8f8045c14ca2a57b6fed62d07c352d030
Opasyan007 <122226079+Opasyan007@users.noreply.github.com>
2025-10-01 15:07:57 +0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,9 +1,16 @@
-document.getElementById("btnLogin").onclick = async () => {
+document.getElementById("btnLogin").addEventListener("click", async (e) => {
+  e.preventDefault();
   try {
-    await signInWithEmailAndPassword(auth, emailEl.value, passEl.value);
+    await signInWithEmailAndPassword(auth, emailEl.value.trim(), passEl.value);
     alert("Вход выполнен");
   } catch (e) {
     alert(`Ошибка: ${e.code}\n${e.message}`);
     console.error(e);
   }
-};
+});
+
+document.getElementById("btnLogout").addEventListener("click", async (e) => {
+  e.preventDefault();
+  await signOut(auth);
+  alert("Вы вышли");
+});
